f7e4f58978ce27de7e3f8db65dc42a0b5bf6b2da
Aisha Mohammad <aisha.mohammad@booking.com>
2025-11-06 21:56:00 +0000

    fix random number generator

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -17,24 +17,28 @@ public class Main {
         System.out.println("Option 1: see forecast");
         int choosenOption = scanner.nextInt();
         if (choosenOption == 1) {
-            //generates random number from 0-4, excluding 4
-            int randomNumber = (int)(Math.random()*4) ;
+            //generates random number from 0, 1, 2
+            int randomNumber = (int)(Math.random()*3) ;
             switch (randomNumber) {
-                case 1:
+                case 0:
                     System.out.println("Today is a rainy day with slow foot traffic. People linger around the cafe creating a really cozy atmosphere.");
                     System.out.println("In demand we have:");
                     System.out.println("Hot drinks - lattes, cappuccinos, hot chocolate, chai");
                     System.out.println("Comfort bakes - warm pastries, banana bread, soup if available.");
-                case 2:
+                    //what does break do here?
+                    break;
+                case 1:
                     System.out.println("Rush hour sucks! Everything is always so fast-paced since everyone wants food to takeout and is in a hurry.");
                     System.out.println("In demand we have:");
                     System.out.println("Quick coffees - americanos, drip/filter, flat whites; pre-batched cold brew.");
                     System.out.println("Grab-and-go food - croissants, breakfast sandwiches, yogurt pots.");
-                case 3:
+                    break;
+                case 2:
                     System.out.println("There's so many people in today on a weekend. The room is full of couples and friend chattering their morning away.");
                     System.out.println("In demand we have:");
                     System.out.println("Specialty drinks - flavored lattes, matcha, iced options even in cooler weather.");
                     System.out.println("Brunch items - avocado toast, pastries, cakes, shareables.");
+                    break;
                 default:
                     break;
             }
